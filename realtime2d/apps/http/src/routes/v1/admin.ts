832df86311123faa8c7369096fabce1ccfd29425
shashwat558 <jainshashwat528@gmail.com>
2024-11-18 22:56:50 +0530

    Initialized prisma

--- realtime2d/apps/http/src/routes/v1/admin.ts
+++ realtime2d/apps/http/src/routes/v1/admin.ts
@@ -5,16 +5,16 @@ export const router = Router();
 
 router.post("/element", (req, res) => {
 
-})
+});
 
 router.put("/element/:elementId", (req, res) => {
     
-})
+});
 
 router.post("/avatar", (req, res) => {
 
-})
+});
 
 router.post("/map", (req, res) => {
     
-})
+});
