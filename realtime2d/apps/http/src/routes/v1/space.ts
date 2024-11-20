87f7ee8e0bc3b0268a03c69db6dbd996c6a80597
shashwat558 <jainshashwat528@gmail.com>
2024-11-20 12:02:05 +0530

    Created add space endpoint

--- realtime2d/apps/http/src/routes/v1/space.ts
+++ realtime2d/apps/http/src/routes/v1/space.ts
@@ -1,10 +1,78 @@
 import { Router } from "express";
+import { CreateSpaceSchema } from "../../types";
+import client from "@repo/db/client";
+import { map } from "zod";
 
 export const spaceRouter = Router();
 
 
-spaceRouter.post("/", (req, res) => {
+spaceRouter.post("/", async (req, res) => {
+    const parsedData = CreateSpaceSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.status(400).json({message: "Validation failed"})
+    }
+    try {
+        if(!parsedData.data?.mapId){
+            const space = await client.space.create({
+                data:{
+                    name: parsedData.data?.name ?? '',
+                    width: parseInt((parsedData.data?.dimensions.split("x")[0]) as string),
+                    height: parseInt((parsedData.data?.dimensions.split("x")[1]) as string),
+                    creatorId: req.userId!
+                }
+            })
+            res.json({spaceId: space.id})
 
+        }
+        
+
+        const map = await client.map.findUnique({
+            where:{
+                id: parsedData.data?.mapId
+            }, select:{
+                mapElement: true,
+                width: true,
+                height: true
+
+            }
+        })
+        if(!map){
+            res.status(400).json({message: "No map found"})
+            return
+        }
+
+        let space = await client.$transaction(async () => {
+            const space = await client.space.create({
+                data:{
+                    name: parsedData.data?.name ?? '',
+                    width: map.width,
+                    height: map.height,
+                    creatorId: req.userId
+
+                }
+            });
+            
+
+            await client.spaceElements.createMany({
+                data: map.mapElement.map(e => ({
+                    spaceId: space.id,
+                    elementId: e.elementId,
+                    x: e.x,
+                    y: e.y
+                    
+                }))
+            })
+            return space;
+        })
+        res.json({spaceId: space.id})
+        
+
+
+        
+
+    } catch (error) {
+        
+    }
 })
 
 spaceRouter.delete("/:spaceId", (req, res) => {
